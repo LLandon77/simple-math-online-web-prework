9ce4fc36ee8d3c41291458db91b8968e70619629
Lura Landon <lura.landon@gmail.com>
2019-06-08 21:43:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,20 +3,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  9-3
 end
 
 def division(num1, num2)
+  6/3 
 end
 
 def multiplication(num1, num2)
+  5*2
 end
 
 def modulo(num1, num2)
+  8%3
 end
 
 def square_root(num)
+  Math.sqrt(9)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  #Hint:  5 + (( 6 * 6 ) / 7 )
 end
